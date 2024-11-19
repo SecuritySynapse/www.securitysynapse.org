3addd696d8a952e358bd7a8793145cff032dc832
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-11-19 09:01:00 -0500

    fix: Correct the docstring comment in slides/weekthirteen/src/subject_contains_false_positive.py.

--- slides/weekthirteen/src/subject_contains_false_positive.py
+++ slides/weekthirteen/src/subject_contains_false_positive.py
@@ -1,4 +1,4 @@
-"""Example of a file that contains a secret key."""
+"""Example of a file that contains a false positive secret key."""
 
 if __name__ == "main":
    dummy_key = "A1B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6"
